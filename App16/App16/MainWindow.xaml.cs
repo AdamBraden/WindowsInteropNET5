5214f8188495997648e882e26e32011c0bfabb80
Adam Braden <adambr@microsoft.com>
2021-04-07 12:25:07 -0700

    Add project files.

---
+++ App16/App16/MainWindow.xaml.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Controls.Primitives;
+using Microsoft.UI.Xaml.Data;
+using Microsoft.UI.Xaml.Input;
+using Microsoft.UI.Xaml.Media;
+using Microsoft.UI.Xaml.Navigation;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Security.Credentials.UI;
+using WinRT;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace App16
+{
+    /// <summary>
+    /// An empty window that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            this.InitializeComponent();
+        }
+
+        private void myButton_Click(object sender, RoutedEventArgs e)
+        {
+            myButton.Content = "Clicked";
+
+            //Interop helpers for non-CoreWindow
+            IntPtr windowHandle = (App.Current as App).WindowHandle;
+            var result = UserConsentVerifierInterop.RequestVerificationForWindowAsync((UInt64)windowHandle, "test");
+        }
+    }
+    [ComImport]
+    [Guid("39E050C3-4E74-441A-8DC0-B81104DF949C")]
+    [InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+    public interface IUserConsentVerifierInterop
+    {
+        // Note: Invoking methods on ComInterfaceType.InterfaceIsIInspectable interfaces
+        // is no longer supported in.NET5, but can be simulated with IUnknown.
+        void GetIids(out int iidCount, out IntPtr iids);
+        void GetRuntimeClassName(out IntPtr className);
+        void GetTrustLevel(out TrustLevel trustLevel);
+        IntPtr RequestVerificationForWindowAsync(UInt64 appWindow, IntPtr hstrMessage, [In] Guid riid);
+    }
+
+    //Helper to initialize UserConsentVerifier
+    public static class UserConsentVerifierInterop
+    {
+        public static IAsyncOperation<UserConsentVerificationResult> RequestVerificationForWindowAsync(UInt64 hWnd, string Message)
+        {
+            Guid guid = typeof(IAsyncOperation<UserConsentVerificationResult>).GUID;
+
+            IUserConsentVerifierInterop userConsentVerifierInterop = UserConsentVerifier.As<IUserConsentVerifierInterop>();
+            var marshalStr = MarshalString.CreateMarshaler(Message);
+            var outPtr = userConsentVerifierInterop.RequestVerificationForWindowAsync(hWnd, MarshalString.GetAbi(marshalStr), guid);
+            return (IAsyncOperation<UserConsentVerificationResult>)IInspectable.FromAbi(outPtr);
+        }
+    }
+
+}
